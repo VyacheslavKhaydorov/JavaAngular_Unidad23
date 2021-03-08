043777a7c1559b1dfafcd329f2a282c92a023405
VyacheslavKhaydorov <78428134+VyacheslavKhaydorov@users.noreply.github.com>
2021-03-08 17:50:31 +0100

    Hola Mundo Gradle

---
+++ holaMundoGradle/src/main/java/holaMundoGradle/HolaMundoApp.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package holaMundoGradle;
+
+/**
+ * @author viach
+ *
+ * HOLA MUNDO!!
+ */
+public class HolaMundoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HOLA MUNDO!!");
+
+	}
+
+}
