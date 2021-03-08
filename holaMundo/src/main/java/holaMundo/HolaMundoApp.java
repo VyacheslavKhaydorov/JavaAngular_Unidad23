18dc944d945a7c7b772f2d220b02ea8935a98559
VyacheslavKhaydorov <78428134+VyacheslavKhaydorov@users.noreply.github.com>
2021-03-08 17:47:40 +0100

    Hola Mundo Maven

---
+++ holaMundo/src/main/java/holaMundo/HolaMundoApp.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package holaMundo;
+
+/**
+ * @author viach
+ *
+ * HOLA MUNDO!!
+ */
+public class HolaMundoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HOLA MUNDO!!");
+
+	}
+
+}
